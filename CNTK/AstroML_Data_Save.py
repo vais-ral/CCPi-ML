431c5d8c7a8ac9ada603966b8bf9d49d7b3d38ec
jakeminns <jm648@kent.ac.uk>
2018-07-13 12:19:50 +0100

    AstroML Data Save

---
+++ CNTK/AstroML_Data_Save.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 12:11:09 2018
+
+@author: lhe39759
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 11 10:21:16 2018
+
+@author: lhe39759
+"""
+import numpy as np
+
+
+
+from tempfile import TemporaryFile
+
+from astroML.datasets import fetch_rrlyrae_combined
+from astroML.utils import split_samples
+from astroML.utils import completeness_contamination
+
+#----------------------------------------------------------------------
+from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=8, usetex=False)
+
+
+
+#############################################################
+
+#############Data Loading & Conversion######################
+
+X, y = fetch_rrlyrae_combined()
+
+np.savetxt('AstroML_Data.txt',X,fmt='%d')
+np.savetxt('AstroML_Labels.txt',y,fmt='%d')
+
+print("Done")
