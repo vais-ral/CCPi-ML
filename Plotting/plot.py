e5d1be5c4e016bcdf9cb24e7def390d3c4bba660
jakeminns <jm648@kent.ac.uk>
2018-07-18 11:46:30 +0100

    Updated Plotting Program

--- Plotting/plot.py
+++ Plotting/plot.py
@@ -6,29 +6,49 @@ Created on Tue Jul 17 14:03:48 2018
 """
 
 import numpy as np
+import sys
+def findLimitIndex(arr,limit,limitLogic):
+    if limitLogic <0 or limitLogic > 2:
+        print("Invalid logic choice options: 0: <=, 1: >=, 2: ==")
+        sys.exit()
+    else:
+ 
+        if limitLogic == 0:
+            filter1 = arr<=limit
+        elif limitLogic == 1:
+            filter1 = arr>=limit
+        elif limitLogic == 2:
+            filter1 = arr==limit
 
-def findDeltaLossLimitIndex(arr,limit):
-    return np.where(arr<=limit)[0][0]
+        if np.any(filter1):
+            return np.where(filter1)[0][0]
+        else:
+            print('Limit of:',limit,', did not find result in array',arr)
+            return 'Stop'
 
-def deltaLossLimit(data,limit):
+
+def dataLimit(data,limit,column,labels,limitLogic):
     length = len(data)
     arr = np.zeros((length,4))
+    del labels[column]
     
     for i in range(length):
-        #data[i][3][1:] 1: misses out nan in delta loss
-        index = findDeltaLossLimitIndex(data[i][3][1:],0.1)
-        #Epoch
-        arr[i][0] = data[i][0][index]
-        #Batch Size
-        arr[i][1] = data[i][1][index]
-        #Loss
-        arr[i][2] = data[i][2][index]
-        #Speed
-        arr[i][3] = data[i][4][index]
 
-    return np.transpose(arr)
+        index = findLimitIndex(data[i][column],limit,limitLogic)
+        if index != 'Stop':
+            columnSearch = np.arange(0,5,1)
+            columnSearch = np.delete(columnSearch,np.where(columnSearch==column)[0][0],0)  
+            itter = 0
+            
+            for n in columnSearch:
+                arr[i][itter] = data[i][n][index]
+                itter +=1
+        else:
+            break
 
-fileCNTK = r'C:\Users\lhe39759\Documents\GitHub\CCPi-ML\CNTK\cntk_data_batchnum_'
+    return np.transpose(arr), labels
+
+#fileCNTK = r'C:\Users\lhe39759\Documents\GitHub\CCPi-ML\CNTK\cntk_data_batchnum_'
 filePyTorch = r'C:\Users\lhe39759\Documents\GitHub\CCPi-ML\PyTorch\PyTorch_data_batchnum_'
 
 cntk_data = []
@@ -36,14 +56,28 @@ pytorch_data = []
 
 for file in range(0,500,10):
     
-    data_cntk = np.genfromtxt(fileCNTK+str(file+1)+'.txt',delimiter=',')    
+   # data_cntk = np.genfromtxt(fileCNTK+str(file+1)+'.txt',delimiter=',')    
     data_pytorch = np.genfromtxt(filePyTorch+str(file+1)+'.txt',delimiter=',')
     
-    cntk_data.append(np.transpose(data_cntk))
+    #cntk_data.append(np.transpose(data_cntk))
     pytorch_data.append(np.transpose(data_pytorch))
     
 
-dl_pytorch = deltaLossLimit(pytorch_data,0.001)
-print(dl_pytorch[1],dl_pytorch[2])
-plt.scatter(dl_pytorch[1],dl_pytorch[2])
+plotx = 0
+ploty = 1
+limit = 100
+columnSearch = 0
+limitLogic = 2
+logic = ['<=','>=','=']
+columnLabels = ['Epochs','BatchSize','Loss','DeltaLoss','Speed']
+Title = "For " + columnLabels[columnSearch] + ' ' + logic[limitLogic] + ' ' + str(limit)+' , '
+
+dl_pytorch, columnLabels = dataLimit(pytorch_data,limit,columnSearch,columnLabels,2)
+plt.title(Title+columnLabels[ploty] + ' Vs ' + columnLabels[plotx])
+
+print(columnLabels,dl_pytorch)
+plt.scatter(dl_pytorch[plotx],dl_pytorch[ploty])
+plt.xlabel(columnLabels[plotx])
+plt.ylabel(columnLabels[ploty])
+plt.ylim(0,0.6)
 plt.show()
